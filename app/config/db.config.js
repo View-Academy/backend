126b254e1961364c8f778fa1bfb87e22b20a6251
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2023-04-19 07:16:56 +0300

    inshallah'

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-   url: "mongodb+srv://Khaled:khaleed2010709307@khaleed.jbeyu.mongodb.net/viweacdmy?authSource=admin&replicaSet=atlas-11jbls-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+   url: "mongodb+srv://doadmin:iI0854W37Uj61kht@db-mongodb-nyc1-65074-adb3fb19.mongo.ondigitalocean.com/viweAcadmy?tls=true&authSource=admin",
+   // url: "mongodb+srv://Khaled:khaleed2010709307@khaleed.jbeyu.mongodb.net/viweacdmy?authSource=admin&replicaSet=atlas-11jbls-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
 };
