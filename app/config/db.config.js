b7bf1d2a979e8b4211b79ce7c9dab635593303dc
Khaleedabuhawwas92 <42714078+Khaleedabuhawwas92@users.noreply.github.com>
2023-05-10 12:06:19 +0300

    dddd

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-   url: "mongodb+srv://doadmin:9BH5246zjY7Q0o1I@db-mongodb-fra1-09535-204b08fd.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-fra1-09535&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+   url: "mongodb+srv://doadmin:show-password@db-mongodb-fra1-09535-204b08fd.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-fra1-09535&tls=true",
    // mongodb+srv://doadmin:pZ301x92v6kbAE78@db-mongodb-fra1-65519-50ebf19e.mongo.ondigitalocean.com/viweAcadmy?tls=true&authSource=admin
    // mongodb+srv://doadmin:pZ301x92v6kbAE78@db-mongodb-fra1-65519-50ebf19e.mongo.ondigitalocean.com/admin?authSource=admin&replicaSet=db-mongodb-fra1-65519&tls=true
    // url: "mongodb+srv://Khaled:khaleed2010709307@khaleed.jbeyu.mongodb.net/viweacdmy?authSource=admin&replicaSet=atlas-11jbls-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
